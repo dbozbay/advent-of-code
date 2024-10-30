bd6a2f1b9dad44df2daf3ddad14c3a77c003b43b
GravDev <107803920+thegravdev@users.noreply.github.com.>
2024-10-30 17:25:21 +0000

    feat: improved efficiency of day2 solutions

--- src/day02/main.py
+++ src/day02/main.py
@@ -1,50 +1,38 @@
-from math import prod
-
-
 def part1():
-    av = {"red": 12, "green": 13, "blue": 14}
+    MAX_COLORS = {"red": 12, "green": 13, "blue": 14}
     sum = 0
     with open("data.txt", "r") as f:
         lines = f.readlines()
-        for idx, line in enumerate(lines):
-            game_id = int(idx + 1)
-            game_results = line.split(":")[1]
-            game_sets = game_results.split(";")
-            game_allowed = True
-            for s in game_sets:
-                set_results = s.split(",")
-                for r in set_results:
-                    count, color = r.strip().split(" ")
-                    if av[color] < int(count):
-                        game_allowed = False
-            if game_allowed:
-                sum += game_id
-            else:
-                pass
+        for line in lines:
+            game_is_allowed = True
+            game_id, game_sets = line.strip().split(": ")
+            game_id = int(game_id[5:])
+            for game_set in game_sets.split("; "):
+                for cubes_in_set in game_set.split(", "):
+                    cube_count, cube_color = cubes_in_set.split(" ")
+                    if int(cube_count) > MAX_COLORS[cube_color]:
+                        game_is_allowed = False
+            if game_is_allowed:
+                sum += int(game_id)
     print("Day 2, Part 1:", sum)
 
 
 def part2():
-    power = 0
+    total = 0
     with open("data.txt", "r") as f:
         lines = f.readlines()
         for line in lines:
-            results = line.split(":")[1]
-            min_colors = {"red": 0, "blue": 0, "green": 0}
-            for i, c in enumerate(results):
-                for color, min_num in min_colors.items():
-                    if results[i:].startswith(color):
-                        count = int(
-                            results[i - 3 : i - 1]
-                            if results[i - 3].isdigit()
-                            else results[i - 2]
-                        )
-                        if count > min_num:
-                            min_colors[color] = count
-                    else:
-                        pass
-            power += prod(min_colors.values())
-    print("Day 2, Part 2:", power)
+            _, game_sets = line.strip().split(": ")
+            max_by_color = {"red": 0, "blue": 0, "green": 0}
+            for game_set in game_sets.split("; "):
+                for cubes_in_set in game_set.split(", "):
+                    cube_count, cube_color = cubes_in_set.split(" ")
+                    max_by_color[cube_color] = max(
+                        max_by_color[cube_color], int(cube_count)
+                    )
+            power = max_by_color["red"] * max_by_color["green"] * max_by_color["blue"]
+            total += power
+    print("Day 2, Part 1:", total)
 
 
 part1()
